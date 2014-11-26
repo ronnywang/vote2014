2da0d43b04e77e4a774691a1680e2f47f7e36889
Ronny Wang <ronnywang@gmail.com>
2014-11-26 15:16:09 +0800

    改成 final 抓到了就可以停止抓了

--- webdata/scripts/update.php
+++ webdata/scripts/update.php
@@ -2,4 +2,9 @@
 
 include(__DIR__ . '/../init.inc.php');
 Pix_Table::$_save_memory = true;
-Vote::updateData();
+// 如果已經抓過 final 了，就不需要再更新了
+if (VoteData::find('final-time')->data) {
+    exit;
+}
+Vote::updateData('running', 'time');
+Vote::updateData('final', 'final-time');
