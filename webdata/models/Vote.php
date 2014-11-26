0d80cdf892b267d735157bf57a7ad9ff43555527
Ronny Wang <ronnywang@gmail.com>
2014-11-26 16:00:59 +0800

    加上 read

--- webdata/models/Vote.php
+++ webdata/models/Vote.php
@@ -8,7 +8,7 @@ class Vote
     public static function getParties()
     {
         if (is_null(self::$parties)) {
-            $fp = fopen(__DIR__ . '/../data/party.csv');
+            $fp = fopen(__DIR__ . '/../data/party.csv', 'r');
             self::$parties = array();
             while ($rows = fgetcsv($fp)) {
                 self::$parties[] = $rows;
