b0129b956583d6e4bab3352e93495d923e4a9efa
Ronny Wang <ronnywang@gmail.com>
2014-11-26 11:05:53 +0800

    加上各選舉人名單

--- webdata/models/Candidate.php
+++ webdata/models/Candidate.php
@@ -24,9 +24,26 @@ class Candidate extends Pix_Table
     {
         $type = substr($id, 0, 2);
         switch ($type) {
-        case 'T1':
-        case 'T2':
+        case 'T1': // 區域縣市議員
+        case 'T2': // 平地原住民縣市議員
+        case 'T3': // 山地原住民縣市議員
             return sprintf("T1%03d%02d", substr($id, 2, 3), substr($id, 5, 2));
+
+        case 'TC': // 縣市長
+            return sprintf("TC%03d", substr($id, 2, 3));
+
+        case 'TD': // 鄉鎮市(原住民區)長
+            return sprintf("TD%03d%02d", substr($id, 2, 3), substr($id, 5, 2));
+
+        case 'T4': // 鄉鎮市區民代表
+        case 'T5': // 鄉鎮市區民代表
+            return sprintf("T4%05d%02d", substr($id, 2, 5), substr($id, 7, 2));
+
+        case 'T6': // 鄉鎮市區民代表
+            return sprintf("T6%05d%02d", substr($id, 2, 5), substr($id, 7, 2));
+
+        case 'TV': // 村里長
+            return sprintf("TV%09d", substr($id, 2, 9));
         }
     }
 }
