1c61c8c59d7cb5cd6fb7e81f3b38efefb0e2accd
Ronny Wang <ronnywang@gmail.com>
2014-11-26 15:53:33 +0800

    加上 cache 時間，除了開票資料 cache 60 秒以外，其他都 cache 一天

--- webdata/controllers/ApiController.php
+++ webdata/controllers/ApiController.php
@@ -10,27 +10,32 @@ class ApiController extends Pix_Controller
 
     public function countyAction()
     {
+        header('Cache-Control: max-age=86400');
         return $this->json(Vote::getCounties());
     }
 
     public function townAction()
     {
+        header('Cache-Control: max-age=86400');
         return $this->json(Vote::getTowns());
     }
 
     public function partyAction()
     {
+        header('Cache-Control: max-age=86400');
         return $this->json(Vote::getParties());
     }
 
     public function dataAction()
     {
+        header('Cache-Control: max-age=60');
         list(, /*api*/, /*data*/, $id) = explode('/', $this->getURI());
         return $this->json(json_decode(VoteData::find($id)->data));
     }
 
     public function candidateAction()
     {
+        header('Cache-Control: max-age=86400');
         list(, /*api*/, /*candidate*/, $id) = explode('/', $this->getURI());
         return $this->json(Candidate::findCandidateByVoteId($id));
     }
