216812380f8a5d154d166830220b37776062e762
Ronny Wang <ronnywang@gmail.com>
2022-08-18 15:31:35 +0800

    Create config.sample.php

---
+++ webdata/config.sample.php
@@ -0,0 +1,7 @@
+<?php
+
+putenv('API_DOMAIN=api.vote2014.g0v.ronny.tw');
+putenv('DATABASE_URL=mysql://user:passwd@127.0.0.1/db');
+putenv('DATA_DIR=/srv/data/');
+putenv('PASSWORD=password'); // 中選會介接密碼
+putenv('USER=user'); // 中選會介接帳號
